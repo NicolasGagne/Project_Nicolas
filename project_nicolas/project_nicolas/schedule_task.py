1ca4cd2c90ba95fd2f10f52e93bea05473f2ba91
NicolasGagne <ngagne966@hotmail.com>
2018-11-29 15:21:04 -0500

    Clean up the file.

--- project_nicolas/project_nicolas/schedule_task.py
+++ project_nicolas/project_nicolas/schedule_task.py
@@ -6,11 +6,13 @@ Call once a day by pythonanywhere task.
 
 import webbrowser
 
+
 def call_check_link():
     """
     Call url to make the site execute the check_link view.
     :return: None
     """
+    print("Call check link form the tasy system") #Note for the server.
     url = "ngagne.pythonanywhere.com/appBookmarks/check_link/"
     webbrowser.open(url)
 
